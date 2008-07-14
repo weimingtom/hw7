bf18e4a7fdc102e19ccff7e5774b42cf22dec7fa
andygivens <andygivens@7e032dc2-ec51-0410-b278-cb4d121e4200>
2008-07-14 01:29:19 +0000

    added

---
+++ trunk/sprites.h
@@ -0,0 +1,13 @@
+/* ------------------------------ hardware.h-----------------------------------
+*
+*   This includes all registries specific to the GBA.
+*   Also:
+*   Sprites, DMA, and Tiles
+*--------------------------------------------------------------------------*/
+
+#define spritesTilesLen 352
+extern const unsigned short spritesTiles[176];
+#define spritesMapLen 32
+extern const unsigned short spritesMap[16];
+#define spritesPalLen 512
+extern const unsigned short spritesPal[256];
